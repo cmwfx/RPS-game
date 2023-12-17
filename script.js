351d3c1e8161c364368e341130f32acf3e9a9e6a
William <atozniche1@gmail.com>
2023-12-17 14:38:20 +0200

    Removed negative element height

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-//create getComputerChoice function *
-//this function will randomly return either 'rock', 'paper', or 'scissors' *
-//create a function that plays a single game *
-//it should take 2 parameters - the player selection and computerSelection *
-//it should then return a string that declares the winner *
-//make the function parameter case sensative *
-//if its a tie replay the round *
-//create function called game which uses the previous function inside this one to play a best of five game that keeps score and reports a winner or looser at the end
 let computerSelection = () => {
 	let randomChoice = Math.floor(Math.random() * 3);
 	if (randomChoice == 0) {
@@ -67,12 +59,6 @@ scissors.addEventListener("click", () => {
 let playGame = (p1Selection, p2Selection) => {
 	let humanSelection = p1Selection.toLowerCase();
 	let compSelection = p2Selection.toLowerCase();
-
-	// while (humanSelection === compSelection) {
-	// 	playerSelection = prompt("Its a Tie! Please make another choice");
-	// 	humanSelection = playerSelection.toLowerCase();
-	// 	compSelection = computerSelection().toLowerCase();
-	// }
 	if (humanSelection === compSelection) {
 		return "It's a Tie! Please make another choice.";
 	} else if (humanSelection == "rock" && compSelection == "paper") {
@@ -89,37 +75,3 @@ let playGame = (p1Selection, p2Selection) => {
 		return "You Lose! Rock beats Scissors";
 	}
 };
-
-//console.log(playGame(playerSelection, computerSelection()));
-// let game = () => {
-// 	let i = 0;
-// 	let j = 0;
-// 	let k = 0;
-// 	while (i < 5 && j < 5) {
-// 		let playerSelection = prompt("What is your choice?");
-// 		let gameResult = playGame(playerSelection, computerSelection());
-// 		if (gameResult.includes("Win")) {
-// 			i++;
-
-// 			if (i == 5) {
-// 				console.log(i);
-// 				console.log(j);
-// 				console.log(k);
-// 				return "You Win";
-// 			}
-// 		} else if (gameResult.includes("Lose")) {
-// 			j++;
-
-// 			if (j == 5) {
-// 				console.log(j);
-// 				console.log(i);
-// 				console.log(k);
-// 				return "Computer wins";
-// 			}
-// 		} else if (gameResult.includes("Tie")) {
-// 			k++;
-// 		}
-// 	}
-// };
-
-// console.log(game());
